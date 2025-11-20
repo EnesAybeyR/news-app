7a4f3b933455fe955eef6c9b4eb20f0ca8b24bef
Enes <enesaybeyR@outlook.com>
2025-11-20 16:47:39 +0300

    .env added

--- newsApi/Program.cs
+++ newsApi/Program.cs
@@ -1,63 +1,63 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using newsApi.Data;
-using newsApi.Services;
-using newsApi.Services.NewsService;
-using Scalar.AspNetCore;
-using FluentEmail.Smtp;
-using System.Net;
-using System.Net.Mail;
+    using System.Text;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.IdentityModel.Tokens;
+    using newsApi.Data;
+    using newsApi.Services;
+    using newsApi.Services.NewsService;
+    using Scalar.AspNetCore;
+    using FluentEmail.Smtp;
+    using System.Net;
+    using System.Net.Mail;
+    using DotNetEnv;
 
 
+    var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=news.db"));
-builder.Services.AddControllers();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<INewsService, NewsService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    Env.Load();
+    builder.Services.AddOpenApi();
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=news.db"));
+    builder.Services.AddControllers();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<INewsService, NewsService>();
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
     {
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["AppSettings:Issuer"],
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["AppSettings:Audience"],
-        ValidateLifetime = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]!)),
-        ValidateIssuerSigningKey = true,
-    };
-});
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = Environment.GetEnvironmentVariable("ISSUER"),
+            ValidateAudience = true,
+            ValidAudience = Environment.GetEnvironmentVariable("AUDIENCE"),
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("TOKEN"))),
+            ValidateIssuerSigningKey = true,
+        };
+    });
 
-builder.Services.AddFluentEmail(builder.Configuration["AppSettings:EMAILNAME"]).AddSmtpSender(new SmtpClient("smtp.gmail.com")
-{
-    Port = 587,
-    Credentials = new NetworkCredential(builder.Configuration["AppSettings:EMAILNAME"], builder.Configuration["AppSettings:EMAILCODE"]),
-    EnableSsl = true,
-});
-
-
-
-var app = builder.Build();
+    builder.Services.AddFluentEmail(Environment.GetEnvironmentVariable("EMAILNAME")).AddSmtpSender(new SmtpClient("smtp.gmail.com")
+    {
+        Port = 587,
+        Credentials = new NetworkCredential(Environment.GetEnvironmentVariable("EMAILNAME"),Environment.GetEnvironmentVariable("EMAILCODE")),
+        EnableSsl = true,
+    });
 
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
-
-app.UseHttpsRedirection();
-app.MapControllers();
-app.UseAuthorization();
+    var app = builder.Build();
 
 
-app.Run();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.UseAuthorization();
+
+
+    app.Run();
 
